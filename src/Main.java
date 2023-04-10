1265b6e1a5e603d4eca18f625b75131b4c2d184b
filippova_t <tanya-fef@yandex.ru>
2023-04-11 00:14:24 +0800

    задача из билета 10 решена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*задача 10
+        Дан массив пар названий книг и авторов, необходимо привести каждую пару в строку вида:
+        “Название книги” Имя Отчество Фамилия автора
+         */
+        String[] array = {
+                "Сказка о рыбаке и рыбке, А.С.Пушкин",
+                "Ася, И.С.Тургенев",
+                "Маскарад, М.Ю.Лермонтов"
+        };
+        for (int i = 0; i < array.length; i++) {
+            String[] part = array[i].split(",");
+            String name = part[0];
+            String author = part[1];
+            array[i] = "\"" + name + "\"" + author;
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
